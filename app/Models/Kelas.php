2de6ffe776cfb80cc8e3305c3771275d8591ac73
iqbalanandra <iqbalcopo70@gmail.com>
2024-10-04 21:51:54 +0700

    update to modul 5

--- app/Models/Kelas.php
+++ app/Models/Kelas.php
@@ -8,10 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Kelas extends Model
 {
     use HasFactory;
+    protected $table = 'kelas';
+    public function getKelas()
+    {
+        return $this->all();
+    }
 
     protected $guarded = ['id'];
 
-    public function user (){
-        return $this -> hasMany(UserModel::class,'kelas_id');
+    public function user()
+    {
+        return $this->hasMany(UserModel::class, 'kelas_id');
     }
 }
